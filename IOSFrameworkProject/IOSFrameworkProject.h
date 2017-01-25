796d79f87fbce4d3bdc59c89befbf7e711043250
Salomé Russier <salome.russier@gmail.com>
2017-01-25 10:21:05 +0100

    First commit

---
+++ IOSFrameworkProject/IOSFrameworkProject.h
@@ -0,0 +1,19 @@
+//
+//  IOSFrameworkProject.h
+//  IOSFrameworkProject
+//
+//  Created by Salomé Russier on 06/01/2017.
+//  Copyright © 2017 Salomé Russier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IOSFrameworkProject.
+FOUNDATION_EXPORT double IOSFrameworkProjectVersionNumber;
+
+//! Project version string for IOSFrameworkProject.
+FOUNDATION_EXPORT const unsigned char IOSFrameworkProjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IOSFrameworkProject/PublicHeader.h>
+
+
