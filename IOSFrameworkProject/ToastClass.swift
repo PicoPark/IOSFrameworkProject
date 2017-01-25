4cf8b95a57cf74679e48d5dda73266fd2d70f8e9
David Fournier <fournierdavid49@yahoo.fr>
2017-01-25 11:46:29 +0100

    First file

---
+++ IOSFrameworkProject/ToastClass.swift
@@ -0,0 +1,9 @@
+//
+//  ToastClass.swift
+//  IOSFrameworkProject
+//
+//  Created by David Fournier on 25/01/2017.
+//  Copyright © 2017 Salomé Russier. All rights reserved.
+//
+
+import Foundation
